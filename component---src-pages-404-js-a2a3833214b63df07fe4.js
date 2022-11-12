22724e4f98947872fb60574fa7f08d305d7a5dce
Jason Woitalla <jason.woitalla@gmail.com>
2022-11-11 21:48:22 -0600

    FABRIK Page

--- component---src-pages-404-js-a2a3833214b63df07fe4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjasonwoitalla=self.webpackChunkjasonwoitalla||[]).push([[883],{4658:function(e,t,a){a.r(t),a.d(t,{default:function(){return r}});var l=a(7294),n=a(2875),r=function(){return l.createElement(n.Z,{pageTitle:"404",fullHeight:"true"},l.createElement("h1",{className:"_404-module--title--mduE6"},"404 Page Not Found"),l.createElement("p",null,"Sorry about that, try clicking a link from the header to redirect to a page that exists."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a2a3833214b63df07fe4.js.map
